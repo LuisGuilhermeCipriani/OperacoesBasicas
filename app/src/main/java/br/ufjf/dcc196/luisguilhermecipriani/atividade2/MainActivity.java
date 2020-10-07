df0a5b0c7aed20a24c4ee60b797fd7039ffc8ebf
Luis Guilherme Cipriani <lgccipriani1@ice.ufjf.br>
2020-10-07 17:42:10 -0300

    Componentes capturados pelos IDs

--- app/src/main/java/br/ufjf/dcc196/luisguilhermecipriani/atividade2/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/luisguilhermecipriani/atividade2/MainActivity.java
@@ -3,8 +3,19 @@ package br.ufjf.dcc196.luisguilhermecipriani.atividade2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText numero1;
+    private EditText numero2;
+    private TextView resultado;
+    private Button somar;
+    private Button subtrair;
+    private Button multiplicar;
+    private Button dividir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +23,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide(); //Esconde a barra superior da tela
+
+        numero1 = findViewById(R.id.editTextNumero1);
+        numero2 = findViewById(R.id.editTextNumero2);
+        resultado = findViewById(R.id.textViewResultado);
+        somar = findViewById(R.id.buttonSomar);
+        subtrair = findViewById(R.id.buttonSubtrair);
+        multiplicar = findViewById(R.id.buttonMultiplicar);
+        dividir = findViewById(R.id.buttonDividir);
     }
 }
